28bd8c6f8fba8b51d8025314ec34e1c266e01862
Conno <nissimconnor@gmail.com>
2026-09-16 07:34:16 -0700

    Test generated native export quote styles [skip ci]

--- tests/singlethread/check-glue.cjs
+++ tests/singlethread/check-glue.cjs
@@ -5,12 +5,14 @@ const {execFileSync} = require('node:child_process');
 const assert = require('node:assert/strict');
 const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'celeste-glue-'));
 try {
-  for (const indent of ['', '  ', '    ']) {
+  for (const quote of ["'", '"']) for (const indent of ['', '  ', '    ']) {
     const file = path.join(dir, 'native.js');
-    fs.writeFileSync(file, `var Module = moduleArg;\ncreateDotnetRuntime = Module = moduleArg(Module);\n${indent}if (Module['preInit']) {\n}\n// Begin runtime exports\nModule['addRunDependency'] = addRunDependency;\n// End runtime exports\nModule['_test'] = wasmExports['test'];\n`);
+    fs.writeFileSync(file, `var Module = moduleArg;\ncreateDotnetRuntime = Module = moduleArg(Module);\n${indent}if (Module['preInit']) {\n}\n// Begin runtime exports\nModule['addRunDependency'] = addRunDependency;\n// End runtime exports\nModule[${quote}_test${quote}] = wasmExports[${quote}test${quote}];\n`);
     execFileSync(process.execPath, [path.join(__dirname, 'modernize-native.cjs'), file]);
     const first = fs.readFileSync(file, 'utf8');
     assert(first.indexOf("Module['addRunDependency'] =") < first.indexOf("if (Module['preInit'])"));
+    assert(first.includes('Module["_test"] = (...args)'));
+    assert(first.indexOf('Module["_test"] = (...args)') < first.indexOf("if (Module['preInit'])"));
     assert(first.includes('var _scriptDir, __dirname, createDotnetRuntime;'));
     execFileSync(process.execPath, [path.join(__dirname, 'modernize-native.cjs'), file]);
     assert.equal(fs.readFileSync(file, 'utf8'), first);
